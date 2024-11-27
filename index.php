6ee4316740e79f92373bdd547de5bf527d49c706
AIC Digital <53875525+PWPankaj@users.noreply.github.com>
2024-11-27 21:17:35 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<html>
+  <body>
+    <h1>This Web Application is created using Azure App Service </h1>
+    <?php
+    echo "<h1>This is a php output from app service!!<h1>";
+    ?>
+  </body>
+</html>
